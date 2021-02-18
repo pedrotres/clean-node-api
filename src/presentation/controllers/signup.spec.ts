008ac7cb458fbae54b2970687eac2293717bac04
pedrotres <52545592+pedrotres@users.noreply.github.com>
2021-02-17 22:09:17 -0300

    test: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -14,5 +14,19 @@ describe('Signup Controller', () => {
       expect(htppResponse.statusCode).toBe(400)
       expect(htppResponse.body).toEqual(new Error('Missing param: name'))
     })
+
+    test('Should return 400 if no email provided', () => {
+      const sut = new SignUpController()
+      const httpRequest = {
+          body: {
+              name: 'any_name',
+              password: 'any_password',
+              passwordConfirmation: 'any_password'
+          }
+      }
+      const htppResponse = sut.handle(httpRequest)
+      expect(htppResponse.statusCode).toBe(400)
+      expect(htppResponse.body).toEqual(new Error('Missing param: email'))
+    })
 })
 
